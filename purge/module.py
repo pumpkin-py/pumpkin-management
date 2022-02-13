88310b899baccc424125ea154b70ab421d744c62
czechbol <adamludes@gmail.com>
2022-02-13 15:57:15 +0100

    purge fix

--- purge/module.py
+++ purge/module.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
 
+from typing import Optional
 
 import nextcord
 from nextcord.ext import commands
-
-from pie import check, i18n, logger
-
+from pie import check, i18n, logger, utils
 
 _ = i18n.Translator("modules/mgmt").translate
 guild_log = logger.Guild.logger()
@@ -22,7 +21,7 @@ class Purge(commands.Cog):
     @commands.guild_only()
     @check.acl2(check.ACLevel.SUBMOD)
     @commands.command(name="purge")
-    async def purge(self, ctx: commands.Context, num_to_delete: int = None):
+    async def purge(self, ctx: commands.Context, count: Optional[int] = None):
         """Purge spam messages.
 
         Either reply to the oldest message you want to keep or provide a number of messages to delete.
@@ -32,21 +31,80 @@ class Purge(commands.Cog):
         channel = ctx.channel
         msg = ctx.message
         if msg.type == nextcord.MessageType.reply:
-            if num_to_delete is not None:
-                await channel.reply(
+            if count is not None:
+                await ctx.reply(
                     _(
                         ctx,
-                        "Please use either a reply or provide a number of messages to delete. Not both",
+                        "Please use either a reply or provide a number of messages to delete. Not both.",
                     )
                 )
                 return
             replied_to = msg.reference.cached_message
             if replied_to is None:
                 replied_to = await ctx.fetch_message(msg.reference.message_id)
-            time = replied_to.created_at
-            deleted = await channel.purge(after=time, check=self._not_pinned)
+
+            embed = utils.discord.create_embed(
+                author=ctx.author,
+                title=_(ctx, "Confirm delete."),
+            )
+            embed.add_field(
+                name=_(ctx, "Delete messages after"),
+                value=replied_to.jump_url,
+                inline=False,
+            )
+            view = utils.ConfirmView(ctx, embed)
+
+            value = await view.send()
+
+            if value is None:
+                await ctx.send(_(ctx, "Confirmation timed out."))
+                return
+            elif value:
+                deleted = await channel.purge(
+                    after=replied_to.created_at,
+                    before=ctx.message.created_at,
+                    check=self._not_pinned,
+                )
+            else:
+                await ctx.send(_(ctx, "Aborted."))
+                return
+
+        elif count is not None:
+            if count > 10:
+                embed = utils.discord.create_embed(
+                    author=ctx.author,
+                    title=_(ctx, "Confirm delete."),
+                )
+                embed.add_field(
+                    name=_(ctx, "Number of messages to delete"),
+                    value=str(count),
+                    inline=False,
+                )
+                view = utils.ConfirmView(ctx, embed)
+
+                value = await view.send()
+
+                if value is None:
+                    await ctx.send(_(ctx, "Confirmation timed out."))
+                    return
+                elif value:
+                    deleted = await channel.purge(
+                        limit=count,
+                        before=ctx.message.created_at,
+                        check=self._not_pinned,
+                    )
+                else:
+                    await ctx.send(_(ctx, "Aborted."))
+                    return
+
         else:
-            deleted = await channel.purge(limit=num_to_delete, check=self._not_pinned)
+            await ctx.reply(
+                _(
+                    ctx,
+                    "Please use either a reply or provide a number of messages to delete.",
+                )
+            )
+            return
 
         await guild_log.info(
             ctx.author,
