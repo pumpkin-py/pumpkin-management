470772cf3647a4dbcd65251bd86a3ef9b94d4dd8
czechbol <adamludes@gmail.com>
2022-02-13 01:37:37 +0100

    purge command

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 __name__ = "mgmt"
 __version__ = "0.0.0"
-__all__ = ("verify", "whois", "sync", "unverify", "react2role", "comments")
+__all__ = ("comments", "purge", "react2role", "sync", "unverify", "verify", "whois")
