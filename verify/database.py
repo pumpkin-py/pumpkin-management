0abebdad8b9171c5d9ff01b0678907b0b6a16633
ArcasCZ <VydraMan@seznam.cz>
2022-10-14 16:43:43 +0200

    [WIP] Verify: Add functions + little tweaks
    - Add basic functions for DB objects.
     - Remove blocked parameter as it's not needed
     - Rename role_id to group_id because of smaller chances of typo / logical error

--- verify/database.py
+++ verify/database.py
@@ -6,7 +6,6 @@ from typing import List, Optional
 
 from sqlalchemy import (
     BigInteger,
-    Boolean,
     Column,
     DateTime,
     Enum,
@@ -55,7 +54,84 @@ class VerifyRule(database.base):
     name = Column(String)
     guild_id = Column(BigInteger)
     groups = relationship(lambda: VerifyGroup, back_populates="rule")
-    message = relationship(lambda: VerifyMessage)
+    message = relationship(lambda: VerifyMessage, back_populates="rule")
+
+    @staticmethod
+    def get(guild_id: int, name: str = None) -> List[VerifyRule]:
+        """Get guild rules.
+
+        Args:
+            guild_id: ID of the guild
+            name: Search name (optional)
+        Returns:
+            List of VerifyRule
+        """
+        query = session.query(VerifyRule).filter_by(guild_id=guild_id)
+
+        if name:
+            query = query.filter(VerifyRule.name.ilike(name))
+
+        return query.all()
+
+    @staticmethod
+    def add(guild_id: int, name: str) -> VerifyRule:
+        """Add VerifyRule to DB. Name must be unique for each guild.
+
+        Args:
+            guild_id: ID of the guild
+            name: Name of the rule
+        Returns:
+            None if same record exists, VerifyRule otherwise.
+
+        """
+        query = (
+            session.query(VerifyRule)
+            .filter_by(guild_id=guild_id)
+            .filter(VerifyRule.name.ilike(name))
+            .one_or_none()
+        )
+
+        if query:
+            return None
+
+        rule = VerifyRule(guild_id=guild_id, name=name)
+
+        session.add(rule)
+        session.commit()
+
+    def add_groups(self, groups: List[int]):
+        """Add groups (Discord roles) to the rule. Skips existing groups.
+
+        Args:
+            groups: List of Discord role IDs.
+        """
+        for group in self.groups:
+            if group.group_id in groups:
+                groups.remove(group.group_id)
+
+        for group in groups:
+            groups.append(VerifyGroup(group_id=group, guild_id=self.guild_id))
+
+        session.commit()
+
+    def remove_groups(self, groups: List[int]):
+        """Add groups (Discord roles) to the rule. Skips existing groups.
+
+        Args:
+            groups: List of Discord role IDs.
+        """
+        for group in self.groups:
+            if group.group_id in groups:
+                self.groups.remove(group.group_id)
+
+        session.commit()
+
+    def delete(self):
+        session.delete(self)
+        session.commit()
+
+    def save(self):
+        session.commit()
 
     def __repr__(self) -> str:
         return (
@@ -78,7 +154,7 @@ class VerifyGroup(database.base):
     """Acts as discord role list for VerifyRule.
 
     :param rule_id: ID of the rule.
-    :param role_id: ID of Discord role to assign.
+    :param group_id: ID of Discord role to assign.
     :param guild_id: Guild ID.
     :param rule: Back reference to VerifyRule.
     """
@@ -87,24 +163,28 @@ class VerifyGroup(database.base):
 
     rule_id = Column(
         Integer,
-        ForeignKey("mgmt_verify_rules.idx", ondelete="CASCADE"),
+        ForeignKey("mgmt_verify_rules.idx", cascade="all, delete-orphan"),
         primary_key=True,
     )
-    role_id = Column(BigInteger, primary_key=True)
+    group_id = Column(BigInteger, primary_key=True)
     guild_id = Column(BigInteger)
     rule = relationship(lambda: VerifyRule, back_populates="groups")
+
+    def delete(self):
+        session.delete(self)
+        session.commit()
 
     def __repr__(self) -> str:
         return (
             f'<VerifyGroup rule_id="{self.rule_id}" '
-            f'role_id="{self.role_id}" guild_id="{self.guild_id}" '
+            f'group_id="{self.group_id}" guild_id="{self.guild_id}" '
             f'rule="{self.rule}">'
         )
 
     def dump(self) -> dict:
         return {
             "rule_id": self.rule_id,
-            "role_id": self.role_id,
+            "group_id": self.group_id,
             "guild_id": self.guild_id,
             "rule": self.rule,
         }
@@ -119,7 +199,7 @@ class VerifyMapping(database.base):
     If the domain is not found, it looks for default mapping (username == "" and domain == "").
     If there are no records found, the user is not allowed to verify.
 
-    To block some user / domain, set blocked to True.
+    To block some user / domain, set Rule to None.
 
     To add default rule for domain, add record with username = "" and domain = "someValue.xy"
     To add default rule for all domains, add record with username = "" and domain = ""
@@ -130,7 +210,6 @@ class VerifyMapping(database.base):
     :param rule_id: ID of rule to assign groups and send message.
     :param username: Part of email before @ (empty string to act as default value).
     :param domain: Part of email after @ (empty string to act as default value).
-    :param blocked: If combination is blocked from verification (default = False).
     :param rule: Relationship with :class:`VerifyRule` based on rule_id.
     """
 
@@ -138,19 +217,107 @@ class VerifyMapping(database.base):
 
     guild_id = Column(BigInteger, primary_key=True)
     rule_id = Column(
-        Integer,
-        ForeignKey("mgmt_verify_rules.idx", ondelete="CASCADE"),
+        Integer, ForeignKey("mgmt_verify_rules.idx", ondelete="CASCADE"), nullable=True
     )
     username = Column(String, primary_key=True)
     domain = Column(String, primary_key=True)
-    blocked = Column(Boolean, default=False)
     rule = relationship(lambda: VerifyRule)
+
+    @staticmethod
+    def add(guild_id: int, username: str, domain: str, rule: VerifyRule):
+        """Add or update mapping.
+
+        If user is empty string, the mapping is considered as domain default.
+        If user and domain is empty, the mapping is considered as guild default.
+
+        If Rule is None, verification is blocked.
+
+        Args:
+            guild_id: Discord ID of the guild.
+            username: Username (part of the email before @)
+            domain: Domain (part of the email after @)
+            rule: VerifyRule applied to the user
+        """
+        mapping = (
+            session.query(VerifyMapping)
+            .filter_by(guild_id=guild_id, username=username, domain=domain)
+            .one_or_none()
+        )
+
+        if not mapping:
+            mapping = VerifyMapping(guild_id=guild_id, username=username, domain=domain)
+
+        mapping.rule = rule
+
+        session.merge(mapping)
+        session.commit()
+
+    @staticmethod
+    def get(
+        guild_id: int, rule: VerifyRule = None, username: str = "", domain: str = ""
+    ) -> List[VerifyMapping]:
+        """Get VerifyMapping.
+
+        Args:
+            guild_id: Discord ID of the guild.
+            rule: Applied rule.
+            username: Username (part of the email before @)
+            domain: Domain (part of the email after @)
+
+        Returns:
+            List of VerifyMapping
+        """
+        query = session.query(VerifyMapping).filter_by(guild_id=guild_id)
+
+        if rule:
+            query = query.filter_by(rule=rule)
+        if username:
+            query = query.filter(VerifyMapping.username.ilike(username))
+        if domain:
+            query = query.filter(VerifyMapping.domain.ilike(domain))
+
+        return query.all()
+
+    @staticmethod
+    def map(guild_id: int, username: str, domain: str) -> Optional[VerifyMapping]:
+        """Maps the given username and domain to the guild rule.
+
+        First it searches for exact username and domain.
+        If the result is not found, it searches for domain only rule.
+        If the result is not found, it searches for global rule.
+        If the result is not found, returns None.
+
+        Args:
+            guild_id: Discord ID of the guild.
+            username: Username to search for (empty string for guild / global rule)
+            domain: Domain to search for (empty string for global rule)
+
+        Returns:
+            VerifyMapping if user is mapped, None otherwise
+
+        """
+
+        query = (
+            session.query(VerifyMapping)
+            .filter_by(guild_id=guild_id, username=username, domain=domain)
+            .one_or_none()
+        )
+
+        if not query:
+            if username:
+                return VerifyMapping.map(guild_id, "", domain)
+            elif domain:
+                return VerifyMapping.map(guild_id, "", "")
+            else:
+                return None
+
+        return query
 
     def __repr__(self) -> str:
         return (
             f'<VerifyMapping guild_id="{self.guild_id}" rule_id="{self.rule_id}" '
             f'username="{self.username}" domain="{self.domain}" '
-            f'blocked="{self.blocked}" rule="{self.rule}">'
+            f'rule="{self.rule}">'
         )
 
     def dump(self) -> dict:
@@ -159,7 +326,6 @@ class VerifyMapping(database.base):
             "rule_id": self.rule_id,
             "username": self.username,
             "domain": self.domain,
-            "blocked": self.blocked,
             "rule": self.rule,
         }
 
@@ -206,10 +372,21 @@ class VerifyMember(database.base):
         code: Optional[str],
         status: VerifyStatus,
     ) -> Optional[VerifyMember]:
-        """Add new member."""
-        if VerifyMember.get_by_member(guild_id, user_id) is not None:
+        """Add new member.
+
+        Args:
+            guild_id: Discord ID of the guild.
+            user_id: Discord ID of the user.
+            address: Email address of the user.
+            code: Verify code.
+            Status: VerifyStatus
+
+        Returns:
+            Null if already exists, otherwise VerifyMember
+        """
+        if VerifyMember.get(guild_id, user_id=user_id):
             return None
-        if VerifyMember.get_by_address(guild_id, address) is not None:
+        if VerifyMember.get(guild_id, address=address):
             return None
 
         member = VerifyMember(
@@ -227,59 +404,32 @@ class VerifyMember(database.base):
         return member
 
     @staticmethod
-    def get_by_member(guild_id: int, user_id: int) -> Optional[VerifyMember]:
-        """Get member."""
-        query = (
-            session.query(VerifyMember)
-            .filter_by(
-                guild_id=guild_id,
-                user_id=user_id,
-            )
-            .one_or_none()
-        )
-        return query
+    def get(
+        guild_id: int, user_id: int = None, address: str = None
+    ) -> List[VerifyMember]:
+        """Get member.
 
-    @staticmethod
-    def get_by_address(guild_id: int, address: str) -> Optional[VerifyMember]:
-        """Get member by their e-mail."""
-        query = (
-            session.query(VerifyMember)
-            .filter_by(
-                guild_id=guild_id,
-                address=address,
-            )
-            .one_or_none()
-        )
-        return query
+        Args:
+            guild_id: Discord ID of the guild.
+            user_id: Discord user ID (optional)
+            address: Email address of the user (optional)
 
-    @classmethod
-    def get_all(cls, guild_id: int) -> List[VerifyMember]:
-        """Get members with e-mail containing given regex filter."""
-        return session.query(cls).filter_by(guild_id=guild_id).all()
+        Returns:
+            List of VerifyMembers
+        """
+        query = session.query(VerifyMember).filter_by(guild_id=guild_id)
 
-    @staticmethod
-    def remove(guild_id: int, user_id: int) -> int:
+        if user_id:
+            query = query.filter_by(user_id=user_id)
+        if address:
+            query = query.filter(VerifyMember.address.ilike(address))
+
+        return query.all()
+
+    def remove(self):
         """Remove member from database."""
-        query = (
-            session.query(VerifyMember)
-            .filter_by(
-                guild_id=guild_id,
-                user_id=user_id,
-            )
-            .delete()
-        )
-        return query
-
-    @staticmethod
-    def update(guild_id: int, user_id: int, status: int) -> Optional[VerifyMember]:
-        """Update member from database."""
-        query = VerifyMember.get_by_member(guild_id, user_id)
-        if not query:
-            return None
-
-        query.status = status
+        session.delete(self)
         session.commit()
-        return query
 
     def save(self):
         session.commit()
@@ -328,6 +478,54 @@ class VerifyMessage(database.base):
     )
     guild_id = Column(BigInteger)
     message = Column(String)
+    rule = relationship(lambda: VerifyRule, back_populates="message")
+
+    @staticmethod
+    def set(guild_id: int, message: str, rule: VerifyRule = None):
+        """Set or update message for rule.
+
+        If rule is None, the message is considered as default.
+
+        Args:
+            guild_id: Discord ID of the guild.
+            message: Text of the message.
+            rule: VerifyRule the message is assigned to.
+        """
+        rule_id = rule.idx if rule else None
+        message = session.query(VerifyMessage).filter_by(
+            quild_id=guild_id, rule_id=rule_id
+        )
+
+        if not message:
+            message = VerifyMessage(guild_id=guild_id, rule_id=rule_id)
+
+        message.message = message
+
+        session.merge(message)
+        session.commit()
+
+    @staticmethod
+    def get(guild_id: int, rule: VerifyRule = None) -> Optional[VerifyMessage]:
+        """Get rule or global message.
+
+        Args:
+            guild_id: Discord ID of the guild.
+            rule: VerifyRule
+
+        Returns:
+            VerifyMessage if found, None otherwise.
+        """
+        query = session.query(VerifyMessage).filter_by(guild_id=guild_id)
+
+        if rule:
+            query = query.filter_by(rule=rule)
+
+        message = query.one_or_none()
+
+        if not message and rule:
+            return VerifyMessage.get(guild_id)
+        else:
+            return message
 
     def __repr__(self) -> str:
         return (
